bc0117895a65c3a86013145576ed4d3ab1780cb0
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-15 10:52:37 -0300

    Fazendo Basic Auth com Passport 2

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { BasicStrategy } from "passport-http";
 import { User } from '../models/User';
+import {Request, Response, NextFunction} from 'express'
 
 const notAuthrizedJson = { status: 401, message: 'Não autorizado' };
 
@@ -10,8 +11,19 @@ passport.use(new BasicStrategy( async (email, password, done) => {
         const user = await User.findOne({
             where: { email, password }
         });
-    }
+
+        if(user) {
+            return done(null, user);
+        };
+    };
+
     return done(notAuthrizedJson, false);
 }));
+
+export const priveteRoute = (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('basic', (err: any, user: any) => {
+        return user ? next() : next(notAuthrizedJson);
+    })(req, res, next);
+}
 
 export default passport;
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { priveteRoute } from '../config/passport';
 import * as ApiController from '../controllers/apiController';
 
 const router = Router();
@@ -7,6 +7,6 @@ const router = Router();
 router.post('/register', ApiController.register);
 router.post('/login', ApiController.login);
 
-router.get('/list', ApiController.list);
+router.get('/list', priveteRoute, ApiController.list);
 
 export default router;
