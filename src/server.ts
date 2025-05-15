22dde8866f1227bbf7ecde068d5e44fc4424e30e
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-15 10:19:14 -0300

    Introdução a lib Passport

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import apiRoutes from './routes/api';
+import passport from 'passport';
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ server.use(cors());
 
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({ extended: true }));
+
+server.use(passport.initialize());
 
 server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
 
